0462b28c79c26837c3d8360913fa4d09a02db722
SanchoMascarenhas <sancho.mascarenhas@hotmail.com>
2017-01-21 00:47:43 +0000

    Player rotation

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Quaternion rot;
+
+	void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        GetComponent<Rigidbody2D>().angularVelocity = 0;
+    }
+
+    void Scream()
+    {
+
+    }
+}
